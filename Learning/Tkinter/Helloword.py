883c6c9aa5997bed67a5c8c23d359c86c4278ca9
Muhammad Younis <125958511+YounisJ@users.noreply.github.com>
2025-01-28 00:07:24 +0500

    Add files via upload

---
+++ Learning/Tkinter/Helloword.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+# Main Window
+root = Tk()
+
+Mylbl = Label(root, text="Hello World!")
+Mylbl.pack()
+
+# Main Window ends here
+root.mainloop()
+
---
+++ Learning/Tkinter/Input_fields.py
@@ -0,0 +1,17 @@
+from tkinter import *
+
+#Starts Here
+root = Tk()
+
+input =  Entry(root)
+#Some Parameters
+#bg = blue
+#fg = "white"
+#width = 50
+#height = 50
+
+
+
+
+#Ends here
+root.mainloop()
