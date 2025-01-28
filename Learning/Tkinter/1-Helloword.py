c1e5f19abd7806f1540afdd2f12f4ea4d58ed391
YounisJ <muhammadyounis.bscskdkf22@iba-suk.edu.pk>
2025-01-28 22:51:45 +0500

    Renamed file

---
+++ Learning/Tkinter/1-Helloword.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+# Main Window
+root = Tk()
+root.title("Hello World Program")
+
+Mylbl = Label(root, text="Hello World!")
+Mylbl.pack()
+
+# Main Window ends here
+root.mainloop()
+
