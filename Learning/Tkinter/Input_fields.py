0cc85b8b3238b7baaf7c3833a15888b794b49780
Muhammad Younis <125958511+YounisJ@users.noreply.github.com>
2025-01-28 22:04:25 +0500

    Update Input_fields.py

--- Learning/Tkinter/Input_fields.py
+++ Learning/Tkinter/Input_fields.py
@@ -4,12 +4,23 @@ from tkinter import *
 root = Tk()
 
 input =  Entry(root)
+input.pack()
 #Some Parameters
 #bg = blue
 #fg = "white"
 #width = 50
 #height = 50
+#borderwidth = 5
+#insert() = put some default text into textfield
+ 
+def getname():
+    #get() = used toget data from input field
+    hello = "Hello " + input.get()
+    lblname = Label(root, text=hello)
+    lblname.pack()
 
+Newbutton = Button(root, text="Get name", command=getname)
+Newbutton.pack()
 
 
 
