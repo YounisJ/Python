ea966d64fd051c9d86e9973013f666c5069c9627
YounisJ <muhammadyounis.bscskdkf22@iba-suk.edu.pk>
2025-01-29 09:43:03 +0500

    Edit

--- Learning/Tkinter/6-Calculater.py
+++ Learning/Tkinter/6-Calculater.py
@@ -3,15 +3,26 @@ from tkinter import *
 rootwindow = Tk()
 rootwindow.title("Calculater")
 
-txtinput = Entry(rootwindow, width=30)
-txtinput.grid(row=0,column=0)
+txtinput = Entry(rootwindow, width=35, borderwidth=5)
+txtinput.grid(row=0,column=0, columnspan=3, padx=10, pady=10)
 
-btn1 = Button(rootwindow, text="1",height=2,width=6)
-btn1.grid(row= 1,column=0)
-btn2 = Button(rootwindow, text="2")
-btn2.grid(row= 1,column=1)
-btn3 = Button(rootwindow, text="3")
-btn3.grid(row= 1,column=2)
+def button_add():
+    return
+
+btn1 = Button(rootwindow, text="1",command=button_add)
+btn2 = Button(rootwindow, text="2",command=button_add)
+btn3 = Button(rootwindow, text="3",command=button_add)
+
+btn4 = Button(rootwindow, text="4",command=button_add)
+btn5 = Button(rootwindow, text="5",command=button_add)
+btn6 = Button(rootwindow, text="6",command=button_add)
+
+btn7 = Button(rootwindow, text="7",command=button_add)
+btn8 = Button(rootwindow, text="8",command=button_add)
+btn9 = Button(rootwindow, text="9",command=button_add)
+
+btn0 = Button(rootwindow, text="0",command=button_add)
+
 
 
 
