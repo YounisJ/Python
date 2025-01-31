ff744408417e57160c134a4dcb7c80dde4bf21ec
YounisJ <muhammadyounis.bscskdkf22@iba-suk.edu.pk>
2025-01-31 11:03:10 +0500

    Files Updated

--- Learning/customtkinter/3-EntryWidget.py
+++ Learning/customtkinter/3-EntryWidget.py
@@ -4,8 +4,25 @@ rootwindow = CTk()
 rootwindow.title("Entry Widgets")
 rootwindow.geometry('600x400')
 
+def submit():
+    lblhello = CTkLabel(
+        rootwindow,
+        font=("Helvetica", 20),
+        text="Hello Mr. " + txtInput.get()
+    )
+    lblhello.pack(pady=10)
 
+btnsubmit = CTkButton(
+    rootwindow,
+    text="Submit",
+    command=submit
+)
+btnsubmit.pack(pady=40)
 
-
+txtInput = CTkEntry(
+    rootwindow,
+    placeholder_text="Please Enter Your name"
+)
+txtInput.pack(pady=20)
 
 rootwindow.mainloop()
