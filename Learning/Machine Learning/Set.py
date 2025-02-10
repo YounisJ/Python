836f98661bfcfc77b00156c2ce9c3e53116e4a37
YounisJ <muhammadyounis.bscskdkf22@iba-suk.edu.pk>
2025-02-10 22:34:03 +0500

    File Updated

--- Learning/Machine Learning/Set.py
+++ Learning/Machine Learning/Set.py
@@ -1,4 +1,14 @@
 # set = {} Random/Unordered & immutable, but add/remove ok, no duplicate
 
+fruits = ["Apple","Orange","Banana"]
 
+# print(dir(fruits)) # to check methods available
+# print(help(fruits)) # for help of this collection
+# print(len(fruits)) # check out the length of the collection
+# print("Apple" in fruits) # check whether this exist in out collection
+# fruits[0] = pineaple # re asign value in our collection
 
+# fruits.add()
+# fruits.remove("")
+# fruits.pop()
+# fruits.clear()
