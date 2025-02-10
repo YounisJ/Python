49f10b9d484d122e34ea6b905cb779bb5ad7c385
YounisJ <muhammadyounis.bscskdkf22@iba-suk.edu.pk>
2025-02-10 22:27:12 +0500

    Files Updated

---
+++ Learning/Machine Learning/List.py
@@ -0,0 +1,17 @@
+# List [] Ordered and changeable, duplicates are OK
+fruits = ["Apple","Orange","Banana"]
+
+# print(dir(fruits)) # to check methods available
+# print(help(fruits)) # for help of this collection
+# print(len(fruits)) # check out the length of the collection
+# print("Apple" in fruits) # check whether this exist in out collection
+# fruits[0] = pineaple # re asign value in our collection
+
+# fruits.append("Pineaple") # Appends the pineapple to the end of list
+# fruits.remove("Apple") # removes that element from list
+# fruits.insert(0,"Coconut") # inserts the element at given index
+# fruits.sort()
+# fruits.reverse()
+# fruits.clear()
+# print(fruits.index("Apple"))
+# print(fruits.count("Apple"))
