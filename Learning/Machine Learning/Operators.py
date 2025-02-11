8a68784892314ab4a77fd12c5e1ab86c1afe0ec0
YounisJ <muhammadyounis.bscskdkf22@iba-suk.edu.pk>
2025-02-11 09:16:30 +0500

    File Updated

--- Learning/Machine Learning/Operators.py
+++ Learning/Machine Learning/Operators.py
@@ -1,5 +1,26 @@
 # Python Operators
 
+x = y = 5
+# + Addition
+print(x+y)
+
+# - Subtraction
+print(x-y)
+
+# * Multiplication
+print(x*y)
+
+# / Division
+print(x/y)
+
+# % Modulus
+print(x%y)
+
+# ** Exponential / Power
+print(x**y)
+
+# // Floor Division 
+print(x//y)
 
 
 
